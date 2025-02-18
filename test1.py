ec4b706def7bd61e2a7be5b910c9f18eab5064a8
PraveenCodess <polisettypraveen09@gmail.com>
2025-02-18 23:52:13 +0530

    new_test1.py

---
+++ test1.py
@@ -0,0 +1,15 @@
+# def func(n):
+#     bool = True
+#     val = n + 1
+#     for i in range(2, val):
+#         if val % i == 0:
+#             bool = False
+#             break
+#     if bool:
+#         return val
+#     else:
+#         return func(val)
+
+
+# n = int(input())
+# print(func(n))
